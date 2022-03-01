dcd0482b1c8fcd879a780d43c9ea3d357c26ebef
JM-EH <jmespana01@gmail.com>
2022-02-28 23:09:27 -0600

    Finished Version

---
+++ script.js
@@ -0,0 +1,8 @@
+let btnGreen = document.querySelector('#green')
+let btnBlue = document.querySelector('#blue')
+let btnRed = document.querySelector('#red')
+let btnUndo = document.querySelector('#undo')
+
+btnGreen.addEventListener('click', () => btnGreen.style.backgroundColor ='#49be25')
+btnBlue.addEventListener('click', () => btnBlue.style.backgroundColor ='#2587be')
+btnRed.addEventListener('click', () => btnRed.style.backgroundColor ='#be4d25')
